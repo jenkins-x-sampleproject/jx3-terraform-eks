b3c28e646d955b3dda967422933c1fdbbee9c539
root <root@ip-172-31-32-89.ec2.internal>
2023-08-18 14:44:58 +0000

    fix: configure cluster repository and project

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ module "eks-jx" {
   vault_user           = var.vault_user
   is_jx2               = false
   jx_git_url           = "https://github.com/jenkins-x-sampleproject/jx3-terraform-eks"
-  jx_bot_username      = var.jxbotaccount
-  jx_bot_token         = var.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
+  jx_bot_username      = "jxbotaccount"
+  jx_bot_token         = "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"
   force_destroy        = var.force_destroy
   nginx_chart_version  = var.nginx_chart_version
   install_kuberhealthy = var.install_kuberhealthy
