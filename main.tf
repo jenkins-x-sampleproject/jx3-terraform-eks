04da9beb7e7184fe6e864721668df650a866c922
root <root@ip-172-31-32-89.ec2.internal>
2023-08-18 14:40:50 +0000

    fix: configure cluster repository and project

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 
 module "eks-jx" {
   source               = "github.com/jenkins-x/terraform-aws-eks-jx?ref=v1.21.5"
-  cluster_version      = var.1.20
+  cluster_version      = "1.20"
   cluster_name         = var.cluster_name
-  region               = var.us-east-1
+  region               = "us-east-1"
   vault_user           = var.vault_user
   is_jx2               = false
-  jx_git_url           = var.https://github.com/jenkins-x-sampleproject/jx3-terraform-eks
+  jx_git_url           = "https://github.com/jenkins-x-sampleproject/jx3-terraform-eks"
   jx_bot_username      = var.jxbotaccount
   jx_bot_token         = var.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
   force_destroy        = var.force_destroy
